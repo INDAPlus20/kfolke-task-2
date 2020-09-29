e781c78ed5208f86f3dabee0a43307ca90cfbef7
klara <kfolke@kth.se>
2020-09-29 09:45:42 +0200

    Add main.rs

--- summeraTal/src/main.rs
+++ summeraTal/src/main.rs
@@ -1,3 +1,30 @@
+//fn main() {
+ //   println!("Hello, world!");
+//}
+
+/***
+ * Template to a Kattis solution.
+ * See: https://open.kattis.com/help/rust
+ * Author: Viola Söderlund <violaso@kth.se>
+ */
+
+use std::io;
+use std::io::prelude::*;
+
+/// Kattis calls main function to run your solution.
 fn main() {
-    println!("Hello, world!");
+    // get standard input stream
+    let input = io::stdin();
+
+    // get input lines as strings
+    let mut lines = input
+        .lock()
+        .lines()
+        .map(|_line| _line.ok().unwrap().to_string())
+        .collect::<Vec<String>>();
+
+    /* add code here ... */
+
+    eprintln!("Kattis skips this comment!");
+    //println!("Print to standard output.");
 }
